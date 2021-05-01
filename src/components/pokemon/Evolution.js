70d0653fbb81ac1a914b0908ea649680dd6a40cd
atou <atou@marinedepot.com>
2021-05-01 01:08:55 -0700

    add split evolutions

--- src/components/pokemon/Evolution.js
+++ src/components/pokemon/Evolution.js
@@ -11,12 +11,12 @@ const Evolution = (props) => {
   if (!data) return <p>Loading...</p>;
   let base = data.chain.species.name;
   let firstEvolution =
-    data.chain.evolves_to.length > 0 ? data.chain.evolves_to[0] : undefined;
+    data.chain.evolves_to.length > 0 ? data.chain.evolves_to : undefined;
   let secondEvolution;
   console.log(data);
   if (data.chain.evolves_to[0] !== undefined) {
     if (data.chain.evolves_to[0].evolves_to[0] !== undefined) {
-      secondEvolution = data.chain.evolves_to[0].evolves_to[0];
+      secondEvolution = data.chain.evolves_to[0].evolves_to;
     }
   }
   return (
@@ -39,10 +39,11 @@ const Evolution = (props) => {
                 d="M11.7071,4.29289 L15.4142,8 L11.7071,11.7071 C11.3166,12.0976 10.6834,12.0976 10.2929,11.7071 C9.90237,11.3166 9.90237,10.6834 10.2929,10.2929 L11.5858,9 L2,9 C1.44771,9 1,8.55228 1,8 C1,7.44772 1.44771,7 2,7 L11.5858,7 L10.2929,5.70711 C9.90237,5.31658 9.90237,4.68342 10.2929,4.29289 C10.6834,3.90237 11.3166,3.90237 11.7071,4.29289 Z"
               />
             </svg>
-            <EvolutionDetails details={firstEvolution.evolution_details[0]} />
+            {/* <EvolutionDetails details={firstEvolution.evolution_details[0]} /> */}
           </div>
           <div className="col">
-            <p>{firstEvolution.species.name}</p>
+            {firstEvolution.map((pokemon) => pokemon.species.name)}
+            {/* <p>{firstEvolution.species.name}</p> */}
           </div>
           {secondEvolution && (
             <>
@@ -59,12 +60,13 @@ const Evolution = (props) => {
                     d="M11.7071,4.29289 L15.4142,8 L11.7071,11.7071 C11.3166,12.0976 10.6834,12.0976 10.2929,11.7071 C9.90237,11.3166 9.90237,10.6834 10.2929,10.2929 L11.5858,9 L2,9 C1.44771,9 1,8.55228 1,8 C1,7.44772 1.44771,7 2,7 L11.5858,7 L10.2929,5.70711 C9.90237,5.31658 9.90237,4.68342 10.2929,4.29289 C10.6834,3.90237 11.3166,3.90237 11.7071,4.29289 Z"
                   />
                 </svg>
-                <EvolutionDetails
+                {/* <EvolutionDetails
                   details={secondEvolution.evolution_details[0]}
-                />
+                /> */}
               </div>
               <div className="col">
-                <p>{secondEvolution.species.name}</p>
+                {secondEvolution.map((pokemon) => pokemon.species.name)}
+                {/* <p>{secondEvolution.species.name}</p> */}
               </div>
             </>
           )}
